3508968141cde70905567efa5bbcee1c9ae29ecc
uganh16 <uganh16@gmail.com>
2025-09-06 21:44:18 +0800

    [sayhello] Use Bootstrap-Flask to integrate Bootstrap

--- projects/sayhello/sayhello/__init__.py
+++ projects/sayhello/sayhello/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap4
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap4(app)
 
 from sayhello import views, errors, commands
